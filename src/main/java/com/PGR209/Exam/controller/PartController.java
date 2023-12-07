f3ad142f33827494d9460ca9eb22a40a79f7d233
Vegard Falldalen <vegfall@gmail.com>
2023-12-07 11:02:54 +0100

    Added part controller with get, put, post and delete

---
+++ src/main/java/com/PGR209/Exam/controller/PartController.java
@@ -0,0 +1,38 @@
+package com.PGR209.Exam.controller;
+
+import com.PGR209.Exam.model.Part;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PartController {
+    @GetMapping("{id}")
+    public Part getPartById() {
+        System.out.println("Part by id");
+        return null;
+    }
+
+    @GetMapping
+    public List<Part> getPartAll() {
+        System.out.println("List of all parts");
+        return new ArrayList<>();
+    }
+
+    @PostMapping
+    public Part newPart(@RequestBody Part part) {
+        System.out.println("Add new part");
+        return part;
+    }
+
+    @DeleteMapping
+    public void deletePart(Long id) {
+        System.out.println("Delete part");
+    }
+
+    @PutMapping
+    public Part updatePart(Part part) {
+        System.out.println("Update part");
+        return part;
+    }
+}

c858d3f0ba26f424c8ae716855ee85c9ea5aab08
Vegard Falldalen <vegfall@gmail.com>
2023-12-07 10:34:23 +0100

    Added post, delete and put to customer controller

--- src/main/java/com/PGR209/Exam/controller/CustomerController.java
+++ src/main/java/com/PGR209/Exam/controller/CustomerController.java
@@ -1,20 +1,47 @@
 package com.PGR209.Exam.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.PGR209.Exam.model.Address;
+import com.PGR209.Exam.model.Customer;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("api/customer")
 public class CustomerController {
     @GetMapping("{id}")
-    public void getCustomerById() {
+    public Customer getCustomerById() {
         System.out.println("Customer by id");
+        return null;
     }
 
     @GetMapping
-    public void getCustomerAll() {
+    public List<Customer> getCustomerAll() {
         System.out.println("List of all customers");
+        return new ArrayList<>();
+    }
+
+    @PostMapping
+    public Customer newCustomer(@RequestBody Customer customer) {
+        System.out.println("Add new customer");
+        return customer;
+    }
+
+    @DeleteMapping
+    public void deleteCustomer(Long id) {
+        System.out.println("Delete customer");
+    }
+
+    @PutMapping
+    public Customer updateCustomer(Customer customer) {
+        System.out.println("Update customer");
+        return customer;
+    }
+
+    @PutMapping
+    public Customer addAddress(Long id, Address address) {
+        System.out.println("Add address to customer");
+        return null;
     }
 }

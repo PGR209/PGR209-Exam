4f5db0fac850b11475be48a01f8eedbc24cf13d1
LHL <louisehjulerlarsen@gmail.com>
2023-12-12 17:41:48 +0100

    delete unused import

--- src/main/java/com/PGR209/Exam/repository/MachineRepository.java
+++ src/main/java/com/PGR209/Exam/repository/MachineRepository.java
@@ -1,6 +1,5 @@
 package com.PGR209.Exam.repository;
 
-import com.PGR209.Exam.model.Address;
 import com.PGR209.Exam.model.Machine;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;

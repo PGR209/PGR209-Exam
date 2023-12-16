3368ddc807962cb47e59494859a55a1160229ff8
Vegard Falldalen <vegfall@gmail.com>
2023-12-16 13:25:24 +0100

    Added address not found exception classes

---
+++ src/main/java/com/PGR209/Exam/controller/exception/ModelNotFoundException.java
@@ -0,0 +1,8 @@
+package com.PGR209.Exam.controller.exception;
+
+import java.io.Serial;
+
+public class ModelNotFoundException extends RuntimeException {
+    @Serial
+    private static final long serialVersionUID = 1L;
+}
